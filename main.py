2ef6b8e1020c2988bad431e58df8cc321b2e31dc
jisung-lee <2zi.sung00@gmail.com>
2024-10-14 18:18:16 +0900

    comment : Add description for main function

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import time
 from codes.wrapper.wrapperTranslator import WrapperTranslator
 
 def main():
+    """Set working directory path"""
     cur_path = os.getcwd()
     code_path = os.path.join(cur_path, 'codes')
     data_path = os.path.join(cur_path, 'data')
     raw_data_folder_path = os.path.join(data_path, 'rawData')
-
+    
+    """Create an instance of WrapperTranslator, specifying the translator type as 'google' or 'papago'"""
     translator = WrapperTranslator(raw_data_folder_path, "google")
     translator.run('test_data.xlsx', data_path)
 
