2a1abb02279783a1f40681e8fe12063a9ba68b66
이지성 <jslee@cheminet.kr>
2024-10-14 17:14:57 +0900

    feat : Add google translator

---
+++ code/translator/googleTranslator.py
@@ -0,0 +1,21 @@
+from googletrans import Translator as GoogleTranslator
+import time
+
+class GoogleTranslator:
+    def __init__(self):
+        self.google_translator = GoogleTranslator()
+        self.counter = 0
+
+    def translateLang(self, text, src_lang, dest_lang):
+        self.counter += 1
+        try:
+            translation = self.google_translator.translate(text, src=src_lang, dest=dest_lang)
+            translation_text = translation.text
+            print(f"{self.counter}: {text}")
+        except:
+            time.sleep(10)
+            translation = self.google_translator.translate(text, src=src_lang, dest=dest_lang)
+            translation_text = translation.text
+            print(f"{self.counter}: {text}")
+            
+        return translation_text
